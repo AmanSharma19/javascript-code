48ea399dfb71e68e5f2fdc6965aeca5b9692251d
Aman Sharma <152012789+AmanSharma19@users.noreply.github.com>
2026-03-19 17:57:37 +0000

    commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
 console.log("Aman")
+const accountId = 144553
+let accountEmail = "aanajbwa@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 not allowed
+console.log(accountId)
+accountEmail = "bdbv@gmaol.com"
+accountCity = "Gurugram"
+accountPassword = 123112
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
+console.table([accountEmail, accountId, 
+    accountPassword, accountCity])
