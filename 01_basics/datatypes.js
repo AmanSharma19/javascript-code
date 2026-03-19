457821e989e27b81cf24f7f25acef697b09930cc
Aman Sharma <152012789+AmanSharma19@users.noreply.github.com>
2026-03-19 18:54:33 +0000

    conversion of datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -2,5 +2,16 @@
 
 // alert(3 + 3) we are using node.js not browser
 
+// number -> 2 to the power of 53
+// bigint -> for larger numbers
+// string -> ""
+// boolean -> true/false
+// null -> standalone value that represents nothing
+// undefined -> standalone value that represents an uninitialized variable
+// symbol -> unique identifiers for objects
+let a = null
+let b
+console.log(a)
+console.log(b)
 
-
+// object -> complex data structures
