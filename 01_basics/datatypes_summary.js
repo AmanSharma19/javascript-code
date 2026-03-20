507acac2c34d515596b12b1cb4230b3ea33b8d7e
Aman Sharma <152012789+AmanSharma19@users.noreply.github.com>
2026-03-20 19:36:41 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,23 @@
+// datatypes are of two types 
+// premitive --> call by value (when we copy any element it's value is copied not the address)
+// non premitive --> call by reference (when we copy any element it's address is copied like if we make any change in the copied values the same will be reflected in the original value)
+
+/*
+there are 7 types of premitive datatypes:
+1. String 
+2. Number
+3. Boolean
+4. null
+5. undefined
+6. Symbol
+7. BigInt
+8. Decimal
+
+there are 3 types of non-premitive datatypes
+1. Array
+2. Object
+3. Function
+
+javascript is a dynamically typed language as it automatically detects the datatype of the variable 
+
+*/
