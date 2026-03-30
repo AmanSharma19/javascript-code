db5ffca9348131e0fcfbfe3b4564ab858a9b625f
Aman Sharma <152012789+AmanSharma19@users.noreply.github.com>
2026-03-30 17:42:59 +0000

    hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -21,6 +21,11 @@ if(true){
 console.log(aman) // it will print 202 because of block scope of let and const but if we use var it will print 100 because of function scope of var
 
 // expression -> when we assign a value to a variable it is called expression
+val(5) // it will give an error because of hoisting but if we declare the function before calling it then it will work fine 
 const val = function(num){
     return num * num
 }
+
+// hoisting -> when we can access the variable or function before declaring it is called hoisting
+// in js we can access the variable declared with var before declaring it but it will be undefined because of hoisting but we can't access the variable declared with let and const before declaring it because of temporal dead zone
+// in js we can access the function declared with function before declaring it because of hoisting but we can't access the function declared with function expression before declaring it because of hoisting
